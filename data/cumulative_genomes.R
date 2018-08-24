73c78237dfb5463564afdcc5ddbf89dfd8addcdb
Jonathan Dursi <jonathan@dursi.ca>
2018-08-24 17:34:49 -0400

    Start adding a little text

--- data/cumulative_genomes.R
+++ data/cumulative_genomes.R
@@ -3,7 +3,7 @@ data <- read.csv('cumulative_no_genomes.csv', header=FALSE)
 names(data) <- c("year", "genomes")
 ggplot(data, aes(x=year, y=genomes)) + geom_line(color="blue") +
     scale_y_log10(name="Genomes Sequenced", breaks=c(10,100,1000,10000,100000,1000000), labels=c("10","100","1,000","10,000","100,000","1,000,000")) +
-    theme_bw() + theme(text = element_text(size=16), aspect.ratio=1./3.) +
+    theme_bw() + theme(text = element_text(size=14), aspect.ratio=1./3.) +
    ylab("Cumulative number Human Genomes Sequenced") + xlab("Year")
 ggsave("cumulative_genomes.png")
 ggsave("cumulative_genomes.pdf")
--- h4hnet.md
+++ h4hnet.md
@@ -2,14 +2,19 @@
 
 ## Executive Summary
 
+## Human Health Research in the Era of Genomics
 
-## Data Science at Scale for Human Health
+
+![Cumulative genomes sequenced over time](graphics/cumulative_genomes.png)
 
 
 ## HPC4Health - A Made-in-Ontario Collaboration
 
+![HPC4Health Architectrue](graphics/HPC4Health_1.png)
 
 ## The H4H Network - Building On Strengths
+
+![H4H Network](graphics/H4HNetwork.png)
 
 
 ## A Generalizable Approach to Science in the Data Age
